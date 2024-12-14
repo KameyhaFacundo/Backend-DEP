02cfaaa86e7e1eebcf5585949acf51285af842ef
Facundo Kameyha <kameyhafacundo@gmail.com>
2024-12-14 11:24:07 -0300

    Agrego el Script de la base de datos, son inserciones

--- archivos/depStock/obtenerMovimientos.php
+++ archivos/depStock/obtenerMovimientos.php
@@ -5,12 +5,14 @@ header('Content-Type: application/json');
 try {
     include 'conexion.php';
 
-    $query = 'SELECT m."IdConcepto", m."FechaMov", m."Cantidad", m."DescripUnidad", m."Unidad", 
+    $query = '
+            SELECT m."IdMovimiento",m."IdConcepto", m."FechaMov", m."Cantidad", m."DescripUnidad", m."Unidad", 
                      a."Articulo", c."Centro", ac."Accion", m."Motivo"
               FROM public."Movimientos" m
               JOIN public."Articulos" a ON m."IdConcepto" = a."IdConcepto"
               JOIN public."Centros" c ON m."IdCentro" = c."IdCentro"
-              JOIN public."Acciones" ac ON m."IdAccion" = ac."IdAccion"';
+              JOIN public."Acciones" ac ON m."IdAccion" = ac."IdAccion"
+              ORDER BY m."IdMovimiento" ASC';
     $stmt = $pdo->query($query);
     $movimientos = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
