a8779689c0c5c7ffd10c62727805fbdb235f679a
Agustin978 <loboagus10@gmail.com>
2024-12-23 12:29:21 -0300

    Agregada una funcion para obtener los movimientos realizados en una oficina a partir del back

--- src/components/helpers/centros.php
+++ src/components/helpers/centros.php
@@ -31,4 +31,35 @@
             throw new Exception('Error en la solicitur: Codigo HTTO: '.$httpCode);
         }
     }
+
+    function obtenerMovimientosPorCentro($idCentro) {
+        $url = BASE_URL.'Backend/MovimientosPorOficina.php?idCentro='.$idCentro;
+
+        $solicitud = curl_init($url);
+        curl_setopt($solicitud, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($solicitud, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
+        $response = curl_exec($solicitud);
+        $httpCode = curl_getinfo($solicitud, CURLINFO_HTTP_CODE);
+
+        if(curl_errno($solicitud)){
+            $errorMenssage = curl_error($solicitud);
+            curl_close($solicitud);
+            throw new Exception('Error en la petición: ' . $errorMenssage);
+        }
+
+        curl_close($solicitud);
+
+        if($httpCode === 200){
+            $result = json_decode($response, true);
+            if(isset($result['success']) && $result['success']===true){
+                return $result['data'];
+            }else
+            {
+                return [];
+            }
+        }else
+        {
+            throw new Exception('Error en la solicitud: Codigo HTTP: '.$httpCode);
+        }
+    }
 ?>
