1b26acc91c0b5e2feed619bcae0ffc4b58565b14
Agustin978 <loboagus10@gmail.com>
2025-01-15 11:53:23 -0300

    Modificada la llamada al css de centros

--- src/components/Views/Centros/Centros.php
+++ src/components/Views/Centros/Centros.php
@@ -1,7 +1,7 @@
 <?php
       //require_once '../../../../config.php';
       //$ruta1 = BASE_URL.'styles';
-      $ruta2= 'centros';
+      $ruta2= 'Centros';
       //$rutaFooter="../../common/";
       require("../../common/header.php");
       require dirname(__DIR__, 2) . '/helpers/centros.php';
