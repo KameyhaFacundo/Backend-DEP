be998090174e86b523992d163b6ab06eb22433e4
Agustin978 <loboagus10@gmail.com>
2025-02-10 09:33:27 -0300

    Modificados los controles para que al querer modificar un usuario por un solo campo, esto se pueda hacer.

--- src/Backend/actualizarUsuario.php
+++ src/Backend/actualizarUsuario.php
@@ -3,48 +3,43 @@ include '../../config.php';
 try {
     include 'conexion.php';
 
-    if (!empty($_POST['usuario']) && !empty($_POST['password']) && !empty($_POST['rol']) && !empty($_POST['idUsuario'])) {
+    if (!empty($_POST['usuario']) && !empty($_POST['rol']) && !empty($_POST['idUsuario'])) {
         $usu = trim($_POST['usuario']);
-        $contra = trim(md5($_POST['password']));
+        //$contra = trim(md5($_POST['password']));
         $rol = $_POST['rol'];
         $idUsuario = $_POST['idUsuario'];
 
-        // ----------------QUERY PARA OBTENER UN USUARIO YA ALMACENADO SI ES QUE EXISTE ---------
-        /*
-        $queryUsuRepetido = 'SELECT "Usuario" FROM "Usuarios" WHERE "Usuario" = :usuario';
-        $stmtUpdateUsu = $pdo->prepare($queryUsuRepetido);
-        $stmtUpdateUsu->bindParam(':usuario', $usu, PDO::PARAM_STR);
-        $stmtUpdateUsu->execute();
+        if(empty($_POST['password'])){
+            // ----------------QUERY MODIFICAR USUARIO SIN CONTRASÑA---------
+            $queryInsert = 'UPDATE "Usuarios" 
+            SET "Usuario" = :usu, 
+                "IdRol" = (SELECT "IdRol" FROM "Roles" WHERE "Rol" = :rol)
+            WHERE "IdUsuario" = :idUsu';   
 
-        $usuario = $stmtUpdateUsu->fetch(PDO::FETCH_ASSOC);
-        if ($usuario) {
-            header('Location:'.BASE_URL.'../Usuarios?error=El+usuario+ya+existe');
-            exit();
-        }*/
+            //Vinculo los parametros para realizar la consulta.
+            $stmtInsert = $pdo->prepare($queryInsert);
+            $stmtInsert->bindParam(':idUsu', $idUsuario, PDO::PARAM_STR);
+            $stmtInsert->bindParam(':usu', $usu, PDO::PARAM_STR);
+            $stmtInsert->bindParam(':rol', $rol, PDO::PARAM_INT);       
+        }
+        else{
+            $contra = trim(md5($_POST['password']));
+            // ----------------QUERY MODIFICAR USUARIO CON CONTRASEÑA---------
+            $queryInsert = 'UPDATE "Usuarios" 
+            SET "Usuario" = :usu, 
+                "IdRol" = (SELECT "IdRol" FROM "Roles" WHERE "Rol" = :rol), 
+                "Password" = :contra   
+            WHERE "IdUsuario" = :idUsu';
+            
+            //Vinculo los parametros para realizar la consulta.
+            $stmtInsert = $pdo->prepare($queryInsert);
+            $stmtInsert->bindParam(':idUsu', $idUsuario, PDO::PARAM_STR);
+            $stmtInsert->bindParam(':usu', $usu, PDO::PARAM_STR);
+            $stmtInsert->bindParam(':rol', $rol, PDO::PARAM_INT);
+            $stmtInsert->bindParam(':contra', $contra, PDO::PARAM_STR);
+        }
+        
 
-        // ----------------QUERY PARA OBTENER IDROL  ---------
-        // $queryIdRol = 'SELECT "IdRol" FROM "Roles" WHERE "Rol" = :rol';
-
-        // $stmtIdRol = $pdo->prepare($queryIdRol);
-        // $stmtIdRol->bindParam(':rol', $rol, PDO::PARAM_INT);
-        // $stmtIdRol->execute();
-
-        // $idRol = $stmtIdRol->fetch(PDO::FETCH_ASSOC);
-
-        // ----------------QUERY MODIFICAR USUARIO ---------
-        $queryInsert = 'UPDATE "Usuarios" 
-        SET "Usuario" = :usu, 
-            "IdRol" = (SELECT "IdRol" FROM "Roles" WHERE "Rol" = :rol), 
-            "Password" = :contra   
-        WHERE "IdUsuario" = :idUsu';
-
-        //Vinculo los parametros para realizar la consulta.
-
-        $stmtInsert = $pdo->prepare($queryInsert);
-        $stmtInsert->bindParam(':idUsu', $idUsuario, PDO::PARAM_STR);
-        $stmtInsert->bindParam(':usu', $usu, PDO::PARAM_STR);
-        $stmtInsert->bindParam(':rol', $rol, PDO::PARAM_INT);
-        $stmtInsert->bindParam(':contra', $contra, PDO::PARAM_STR);
         
         if ($stmtInsert->execute()) {
             header('Location: '.BASE_URL.'../Usuarios');
